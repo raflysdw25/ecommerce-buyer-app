7dc101df680e1a2cac725d8cab3e756429e875e4
Muhammad Rafly Sadewa <msadewa@pajak.io>
2023-12-02 10:48:22 +0700

    install sentry (and config), and install vuefire + firebase

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
+<script setup>
+const { $sentryCaptureException } = useNuxtApp();
+
+onMounted(() => {
+  $sentryCaptureException("Sentry Buyer App Exception");
+});
+</script>
